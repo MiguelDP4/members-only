056d3f1320fdb88ae735ecdb2c8a65e401ff974e
MiguelDP4 <migueldp4@gmail.com>
2020-03-26 15:10:08 -0600

    Create logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to current_user
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      respond_to do |format|
+        format.html { redirect_to current_user, notice: "User was logged correctly." }
+        log_in @user
+      end
     else
       flash.now[:danger] = "Invalid email/password combination"
       render "new"
     end
   end
+
+  def destroy
+    #log_out
+    #redirect_to root_url
+  end 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
 module SessionsHelper
   def log_in(user)
+    user.remember_token = User.new_token
+    cookies.permanent[:remember_token] = user.remember_token
     session[:user_id] = user.id
+    user.save
   end
 
   def current_user
@@ -8,4 +11,10 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  def log_out
+    cookies[:remember_token] = nil
+    session.delete(:user_id)
+    @current_user = nil
+  end 
 end
